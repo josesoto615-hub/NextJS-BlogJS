fe782b21a285b26b06e66075756441b767871cd1
Jose Soto <josesoto615@gmail.com>
2021-02-19 14:37:53 -0300

    modificación index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
-import { getSortedPostsData } from '../lib/posts'
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+//import { getSortedPostsData } from '../lib/posts'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hi! I'm superior technician in computer science, self-taught and lover of web development.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
 }
